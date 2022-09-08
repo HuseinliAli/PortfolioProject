f9f86e20d5662d62349ef085d44de49e8eade79a
HuseinliAli <ali.guseynoov@gmail.com>
2022-09-09 01:02:29 +0400

    Test

--- CoreProject/Entities/Concrete/ContactAddress.cs
+++ CoreProject/Entities/Concrete/ContactAddress.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Abstract;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Entities.Concrete
 {
     public class ContactAddress : IEntity
     {
+        [Key]
         public int ContactAddressID { get; set; }
 
         public string ContactAddressTitle { get; set; }
--- CoreProject/Entities/Concrete/SocialMedia.cs
+++ CoreProject/Entities/Concrete/SocialMedia.cs
@@ -1,6 +1,7 @@
 ﻿using Entities.Abstract;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,6 +10,7 @@ namespace Entities.Concrete
 {
     public class SocialMedia : IEntity
     {
+        [Key]
         public int SocialMediaID { get; set; }
 
         public string SocialMediaName { get; set; }
